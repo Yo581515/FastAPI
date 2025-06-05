bd22d0ecbf7929b7c427be70396335f39f173921
Yo501818 <581515@stud.hv.no>
2025-06-05 20:09:30 +0200

    added postgres connectetion, tested and working

--- Project 3/TodoApp/database.py
+++ Project 3/TodoApp/database.py
@@ -9,3 +9,30 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+
+# POSTGRES IMPLEMENTATION, TESETED AND WORKING
+'''from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# Replace with your actual values
+POSTGRES_USER = "postgres"
+POSTGRES_PASSWORD = "123456aa"
+POSTGRES_DB = "TodoApplicaionDatabase"
+POSTGRES_HOST = "localhost"  # or IP address
+POSTGRES_PORT = "5432"  # default port
+POSTGRES_SCHEMA = "todos_schema"
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+# Create schema-aware metadata
+metadata = MetaData(schema=POSTGRES_SCHEMA)
+
+# Set schema-aware Base
+Base = declarative_base(metadata=metadata)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)'''
+
